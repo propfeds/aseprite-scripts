badc354d4e17a89f0a1c59cf9915a6c1f04d3dcc
PropFeds <propfeds@gmail.com>
2019-10-02 21:39:37 +0700

    Logic

--- terminal_blues_export.lua
+++ terminal_blues_export.lua
@@ -2,15 +2,17 @@ local directory='D:/Google Drive (7r4nm1nh@gmail.com)/Art/Digital/2019/Terminal 
 local sprite=app.activeSprite
 
 function export(sprite, directory, variant, equipment)
+    local equipment_layer=sprite.layers[7].layers[1]
+    local equipment_prev_state=equipment_layer.isVisible
+
     if equipment
     then
-        local equipment_layer=sprite.layers[7].layers[1]
-        local prev_visible_state=equipment_layer.isVisible
         equipment_layer.isVisible=true
         app.command.SaveFileCopyAs{ useUI="false", filename=directory..variant..'/equipment/mini16.bmp', slice="mini16" }
         app.command.SaveFileCopyAs{ useUI="false", filename=directory..variant..'/equipment/minif16.bmp', slice="minif16" }
-        equipment_layer.isVisible=false
     end
+
+    equipment_layer.isVisible=false
     app.command.SaveFileCopyAs{ useUI="false", filename=directory..variant..'/gfx/alphabet_brass.bmp', slice="alphabet_brass" }
     app.command.SaveFileCopyAs{ useUI="false", filename=directory..variant..'/gfx/alphabet_classic.bmp', slice="alphabet_classic" }
     app.command.SaveFileCopyAs{ useUI="false", filename=directory..variant..'/gfx/alphabet_heavy.bmp', slice="alphabet_heavy" }
@@ -20,9 +22,11 @@ function export(sprite, directory, variant, equipment)
     app.command.SaveFileCopyAs{ useUI="false", filename=directory..variant..'/gfx/mini16.bmp', slice="mini16" }
     app.command.SaveFileCopyAs{ useUI="false", filename=directory..variant..'/gfx/minif16.bmp', slice="minif16" }
     app.command.SaveFileCopyAs{ useUI="false", filename=directory..variant..'/gfx/sprite16.bmp', slice="sprite16" }
+
+    equipment_layer.isVisible=equipment_prev_state
 end
 
-export(sprite, directory, 'blues')
+export(sprite, directory, 'blues', true)
 --[[
 export(sprite, 'D:/Games/Roguelikes/', 'powder118_win')
 --]]
