af5937cfb46c63ca9cc0760b7e100c0b7fd2727c
PropFeds <propfeds@gmail.com>
2019-12-31 01:55:07 +0700

    Shortened slice saving code

--- terminal_blues/export.lua
+++ terminal_blues/export.lua
@@ -1,4 +1,27 @@
-function export(sprite, directory, equipment, extra_alphabets)
+--[[
+Errors
+--]]
+local sprite=app.activeSprite
+if not sprite then
+    app.alert{
+        title='Error',
+        text='Active sprite is nil. Export what?'
+    }
+    return
+end
+
+local function save_slice(slice_name, directory, subdir, file_name)
+    subdir=subdir or ''
+    file_name=file_name or slice_name
+
+    app.command.SaveFileCopyAs{
+        useUI='false',
+        filename=directory..subdir..file_name..'.bmp',
+        slice=slice_name
+    }
+end
+
+local function export(sprite, directory, equipment, extra_alphabets)
     local layer_ref_under=sprite.layers[3]
     local layer_ref_over=sprite.layers[6]
     local prev_state_ref_under=layer_ref_under.isVisible
@@ -10,64 +33,20 @@ function export(sprite, directory, equipment, extra_alphabets)
     local prev_state_equipment=layer_equipment.isVisible
     if equipment then
         layer_equipment.isVisible=true
-        app.command.SaveFileCopyAs{
-            useUI='false',
-            filename=directory..'equipment/mini16.bmp',
-            slice='mini16'
-        }
-        app.command.SaveFileCopyAs{
-            useUI='false',
-            filename=directory..'equipment/minif16.bmp',
-            slice='minif16'
-        }
+        save_slice('mini16', directory, 'equipment/')
+        save_slice('minif16', directory, 'equipment/')
     end
     layer_equipment.isVisible=false
 
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'alphabet_brass.bmp',
-        slice='alphabet_brass'
-    }
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'alphabet_classic.bmp',
-        slice='alphabet_classic'
-    }
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'alphabet_heavy.bmp',
-        slice='alphabet_heavy'
-    }
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'alphabet_light.bmp',
-        slice='alphabet_light'
-    }
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'alphabet_shadow.bmp',
-        slice='alphabet_shadow'
-    }
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'dungeon16.bmp',
-        slice='dungeon16'
-    }
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'mini16.bmp',
-        slice='mini16'
-    }
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'minif16.bmp',
-        slice='minif16'
-    }
-    app.command.SaveFileCopyAs{
-        useUI='false',
-        filename=directory..'sprite16.bmp',
-        slice='sprite16'
-    }
+    save_slice('alphabet_brass', directory)
+    save_slice('alphabet_classic', directory)
+    save_slice('alphabet_heavy', directory)
+    save_slice('alphabet_light', directory)
+    save_slice('alphabet_shadow', directory)
+    save_slice('dungeon16', directory)
+    save_slice('mini16', directory)
+    save_slice('minif16', directory)
+    save_slice('sprite16', directory)
 
     local layer_extra_alphabets=sprite.layers[4].layers[1].layers[6]
     local prev_state_alphabets=layer_extra_alphabets.isVisible
@@ -75,21 +54,18 @@ function export(sprite, directory, equipment, extra_alphabets)
         layer_extra_alphabets.isVisible=true
         for i, layer in ipairs(layer_extra_alphabets.layers) do
             if layer.isVisible==true then
-                app.command.SaveFileCopyAs{
-                    useUI='false',
-                    filename=directory..'extra_alphabets/alphabet_'..layer.name..'.bmp',
-                    slice='alphabet_extra_'..layer.name
-                }
+                save_slice('alphabet_extra_'..layer.name, directory, 'extra_alphabets/', 'alphabet_'..layer.name)
             end
         end
     end
     layer_extra_alphabets.isVisible=prev_state_alphabets
+
     layer_equipment.isVisible=prev_state_equipment
+    
     layer_ref_under.isVisible=prev_state_ref_under
     layer_ref_over.isVisible=prev_state_ref_over
 end
 
-local sprite=app.activeSprite
 local project_directory=sprite.filename:match("(.*[/\\])")
 app.transaction(
     function()
@@ -97,10 +73,13 @@ app.transaction(
         export(sprite, 'D:/Games/Roguelikes/powder118_win/gfx/', false, false)
         --]]
 
-        sprite:loadPalette(project_directory..'palettes/blues.gpl')
+        sprite:loadPalette(project_directory..'data/palettes/blues.gpl')
         export(sprite, project_directory..'export/blues/', true, true)
-        
-        sprite:loadPalette(project_directory..'palettes/prot.gpl')
+
+        sprite:loadPalette(project_directory..'data/palettes/prot.gpl')
         export(sprite, project_directory..'export/protea/', true, true)
+
+        sprite:loadPalette(project_directory..'data/palettes/deut.gpl')
+        export(sprite, project_directory..'export/deuterium/', true, true)
     end
 )
